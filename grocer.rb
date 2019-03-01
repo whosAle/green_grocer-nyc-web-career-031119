a3a94acec7cb7cd5f341b81a3f6ac0023e199082
A Belgrave <abelgrave95@gmail.com>
2019-03-01 04:34:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  cart.each { |k,v| v[:price] *= 0.8 if v[:clearance]}
+  cart.each { |k,v| v[:price] = (v[:price]*0.8).round(1) if v[:clearance]}
   cart
 end
 
